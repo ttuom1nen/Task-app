b678c524c08fde48ba6c640a6c09c10692704519
ttuom1nen <ttuominen@outlook.com>
2021-02-03 22:27:02 +0200

    Add more validation check examples

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,68 +7,80 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error();
-      }
-    },
-  },
-});
-
-const teppoman = new User({ name: "Teppo", age: 20, email: "teppo@" });
-
-teppoman
-  .save()
-  .then(() => {
-    console.log(teppoman);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-// const Task = mongoose.model("Task", {
-//   description: {
+// const User = mongoose.model("User", {
+//   name: {
 //     type: String,
 //     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
 //     validate(value) {
-//       if (value.length < 5) {
-//         throw new Error("Description is too short!");
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Email is invalid");
 //       }
 //     },
 //   },
-//   completed: {
-//     type: Boolean,
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error();
+//       }
+//     },
+//   },
+//   password: {
+//     type: String,
 //     required: true,
+//     minlength: 7,
+//     trim: true,
+//     validate(value) {
+//       if (value.toLowerCase().includes("password")) {
+//         throw new Error("Password cannot contain 'password'");
+//       }
+//     },
 //   },
 // });
 
-// const task = new Task({ description: "Rosi", completed: true });
+// const teppoman = new User({
+//   name: "Teppo",
+//   age: 20,
+//   email: "teppo@teppoman.com",
+//   password: "a0fd0241-697d-58aa-870f-af62f31f511c",
+// });
 
-// task
+// teppoman
 //   .save()
 //   .then(() => {
-//     console.log(task);
+//     console.log(teppoman);
 //   })
 //   .catch((error) => {
 //     console.log(error);
 //   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const task = new Task({ description: "Checking synergies", completed: true });
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
