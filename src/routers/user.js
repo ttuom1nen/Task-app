e8b8738d034806789bd5276df8c72659c165011f
ttuom1nen <ttuominen@outlook.com>
2021-02-14 22:45:53 +0200

    Remove get user by id

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,22 +61,6 @@ router.post("/users/logoutAll", auth, async (req, res) => {
   }
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
 router.patch("/users/:id", async (req, res) => {
   const _id = req.params.id;
   const _body = req.body;
