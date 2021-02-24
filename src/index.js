212b174c04b9f9ae3a01cf5c72a312402b9f403e
ttuom1nen <ttuominen@outlook.com>
2021-02-24 19:42:25 +0200

    Add word document upload endpoint

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 10000000, // 1mb
+  },
+  fileFilter(req, file, callBack) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return callBack(new Error("Please upload a Word document"));
+    }
+
+    callBack(undefined, true);
+  },
+});
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
