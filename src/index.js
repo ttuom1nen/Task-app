5dcfef7438f1751e4bbb014d8512474d9363e55c
ttuom1nen <ttuominen@outlook.com>
2021-02-25 22:34:33 +0200

    Remove file upload test

--- src/index.js
+++ src/index.js
@@ -7,32 +7,6 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 10000000, // 1mb
-  },
-  fileFilter(req, file, callBack) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return callBack(new Error("Please upload a Word document"));
-    }
-
-    callBack(undefined, true);
-  },
-});
-
-app.post(
-  "/upload",
-  upload.single("upload"),
-  (req, res) => {
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.send(400).send({ error: error.message });
-  }
-);
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
