6447e1a89c8caec46d40b427ec749fd6e4feb507
ttuom1nen <ttuominen@outlook.com>
2021-02-28 09:31:49 +0200

    Implement welcome email sending

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,9 +3,15 @@ const sendgridAPIKey = "e2d2e9c7-6d24-5613-8d83-316582a1cb49";
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-  to: "tester@email.com",
-  from: "other.tester@email.com",
-  subject: "This is the subject",
-  text: "This is the body of the email",
-});
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "other.tester@email.com",
+    subject: "Thank you for joining in!",
+    text: `Welcome to the app, ${name}!`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+};
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,14 @@ const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
+const { sendWelcomeEmail } = require("../emails/account");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
