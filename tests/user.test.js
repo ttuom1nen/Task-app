4a14972cd27427da51a09aff6ad030cdf0ad821f
ttuom1nen <ttuominen@outlook.com>
2021-04-11 22:13:22 +0300

    Add tests for avatar and update user

--- tests/user.test.js
+++ tests/user.test.js
@@ -46,8 +46,7 @@ test("Should signup a new user", async () => {
   });
 
   // Assert that the password was hashed
-  expect(user.password).not.toBe("Cape Verde!")
-
+  expect(user.password).not.toBe("Cape Verde!");
 });
 
 test("Should login existing user", async () => {
@@ -59,8 +58,8 @@ test("Should login existing user", async () => {
     })
     .expect(200);
 
-    const user = await User.findById(userOneId)
-    expect(response.body.token).toBe(user.tokens[0].token)
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[0].token);
 });
 
 test("Should not login nonexistent user", async () => {
@@ -92,10 +91,44 @@ test("Should delete account for user", async () => {
     .send()
     .expect(200);
 
-    const user = await User.findById(userOneId);
-    expect(user).toBeNull();
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
 });
 
 test("Should not delete account for unauthorized user", async () => {
   await request(app).delete("/users/me").send().expect(401);
+});
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/sample_image.png")
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Jess",
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual("Jess");
+});
+
+test("Should not update invalid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: "Helsinki",
+    })
+    .expect(400);
 });
