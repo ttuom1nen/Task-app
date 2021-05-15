13b3dbb42b63bb6bffe3f3f7fc56833d7f409be1
ttuom1nen <ttuominen@outlook.com>
2021-05-15 23:07:44 +0300

    Remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import { useEffect } from "react";
 import LoginForm from "./components/LoginForm";
 import { Grid, Segment } from "semantic-ui-react";
 import "./App.css";
 
 function App() {
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await fetch(
-  //       "http://localhost:4000/tasks?sortBy=createdAt:desc",
-  //       {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       }
-  //     );
-
-  //     const data = await response.json();
-  //     console.log(data);
-  //   })();
-  // }, []);
-
   return (
     <Grid columns="equal">
       <Grid.Row>
