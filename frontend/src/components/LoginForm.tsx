bab66e694e47f3b067a6f5b015a4042277c056b4
ttuom1nen <ttuominen@outlook.com>
2021-07-13 23:26:04 +0300

    Refactor login form logic

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,31 +1,42 @@
-import authenticate from "../services/auth.service";
+// import authenticate from "../services/_auth.service";
 // import { RootState } from "../state/store";
+import React, { useState } from "react";
 import { Container, Header, Form, Button } from "semantic-ui-react";
 import { /*useAppSelector,*/ useAppDispatch } from "../state/hooks";
-import { setName } from "../state/userSlice";
-import { LoginInfo } from "../models";
+import { setCredentials } from "../state/authSlice";
+import { LoginRequest /*, LoginResponse*/ } from "../models";
+import { useLoginMutation } from "../services/auth.service";
 
 const LoginForm = () => {
   // const name = useAppSelector((state: RootState) => state.user.name);
   const dispatch = useAppDispatch();
 
+  const [formState, setFormState] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const loginUser = async () => {
+    try {
+      const user = await login(formState).unwrap();
+      dispatch(setCredentials(user));
+      // TODO: push('/')
+    } catch (err) {
+      // TODO: Show error toast
+    }
+  };
+
   const handleSubmit = (e: React.SyntheticEvent): void => {
     e.preventDefault();
-    const target = e.target as typeof e.target & {
-      email: { value: string };
-      password: { value: string };
-    };
-
-    const email = target.email.value;
-    const password = target.password.value;
-
-    authenticate({ email, password }).then((loginInfo: LoginInfo) => {
-      if (loginInfo) {
-        const { user } = loginInfo;
-        dispatch(setName(user.name));
-      }
-    });
+    loginUser();
   };
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setFormState((prev) => ({ ...prev, [name]: value }));
 
   return (
     <Container>
@@ -33,11 +44,21 @@ const LoginForm = () => {
       <Form onSubmit={(e) => handleSubmit(e)}>
         <Form.Field>
           <label>Username</label>
-          <input type="text" placeholder="Email" name="email" />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          />
         </Form.Field>
         <Form.Field>
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
         </Form.Field>
         <Button type="submit">Submit</Button>
       </Form>
