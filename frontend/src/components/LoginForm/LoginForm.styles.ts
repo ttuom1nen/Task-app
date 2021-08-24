633f6c7c7a7f0f2d0f5a21846704223e53ed4576
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:25:07 +0300

    Refactor using styled components

---
+++ frontend/src/components/LoginForm/LoginForm.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  color: white;
+`;
---
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useAppDispatch } from "../../hooks/useStore";
+import { setCredentials } from "../../state/authSlice";
+import { LoginRequest } from "../../models";
+import { useLoginMutation } from "../../services/auth.service";
+import { useHistory } from "react-router-dom";
+import { PaddedContainer } from "../Common/Common.styles";
+import { CustomButton } from "../Common/CustomButton.styles";
+import { CustomInput } from "../Common/CustomInput.styles";
+import { Form } from "./LoginForm.styles";
+
+const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const { push } = useHistory();
+
+  const [formState, setFormState] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const loginUser = async () => {
+    try {
+      const user = await login(formState).unwrap();
+      dispatch(setCredentials(user));
+      push("/");
+    } catch (e) {
+      console.error(e);
+      // TODO: Show error toast
+    }
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault();
+    loginUser();
+  };
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setFormState((prev) => ({ ...prev, [name]: value }));
+
+  if (isLoading) {
+    return <p>Logging in</p>;
+  }
+
+  return (
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <PaddedContainer>
+        <CustomInput
+          name="email"
+          autoComplete="username"
+          id="username"
+          onChange={handleChange}
+        ></CustomInput>
+      </PaddedContainer>
+      <PaddedContainer>
+        <CustomInput
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          id="current-password"
+          onChange={handleChange}
+        ></CustomInput>
+      </PaddedContainer>
+      <PaddedContainer>
+        <CustomButton type="submit" onSubmit={(e) => handleSubmit(e)}>
+          Login
+        </CustomButton>
+      </PaddedContainer>
+    </Form>
+  );
+};
+
+export default LoginForm;
