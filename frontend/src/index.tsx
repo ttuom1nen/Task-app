1356e7a74862998d927d8b859f12fbdc2407c066
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:24:41 +0300

    Remove semantic css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 import { store } from "./state/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
-import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
