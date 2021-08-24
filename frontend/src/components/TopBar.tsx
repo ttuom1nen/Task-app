f0e75b38757b891bea1fa57df44994b9b986e9da
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:26:53 +0300

    First implementation of main page

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopBar = () => {
+  return <div>This is top bar</div>;
+};
+
+export default TopBar;
---
+++ frontend/src/views/MainView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "../components/TopBar";
+
+const MainView = () => {
+  return (
+    <>
+      <TopBar></TopBar>
+      <main>Main content</main>
+    </>
+  );
+};
+
+export default MainView;
