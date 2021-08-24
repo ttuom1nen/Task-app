498b058579cac2a989ce4fdd839ded9c89eefe66
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:25:46 +0300

    Refactor for styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,30 @@
+import React, { useState } from "react";
 import LoginView from "./views/LoginView";
+import MainView from "./views/MainView";
 import { PrivateRoute } from "./utils/PrivateRoute";
 import { Switch, Route } from "react-router-dom";
-
-// import "./App.css";
-
-function Hooray() {
-  return <p>Jee</p>;
-}
+import { ThemeProvider } from "styled-components";
+// import defaultTheme from "./themes/default.theme";
+import { defaultTheme, darkTheme } from "./themes/themes";
 
 function App() {
+  const [theme, setTheme] = useState<any>(defaultTheme);
+
+  const switchTheme = () => {
+    console.log("switchTheme");
+    setTheme(darkTheme);
+  };
+
   return (
-    <Switch>
-      <Route exact path="/login" component={LoginView} />
-      <PrivateRoute path="/">
-        <Hooray></Hooray>
-      </PrivateRoute>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <button onClick={switchTheme}>switch</button>
+      <Switch>
+        <Route exact path="/login" component={LoginView} />
+        <PrivateRoute path="/">
+          <MainView></MainView>
+        </PrivateRoute>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
