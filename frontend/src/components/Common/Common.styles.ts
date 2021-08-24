01664a824b4facb736989b14bf28402681a18eb7
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:23:54 +0300

    Create common styled components

---
+++ frontend/src/components/Common/Common.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainContent = styled.main`
+  display: flex;
+  align-items: center;
+`;
+
+export const PaddedContainer = styled.div`
+  padding: 1rem;
+`;
