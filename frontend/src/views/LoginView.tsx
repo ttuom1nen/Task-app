88ac852283df86ad30577abd0af8a11350e50a6f
ttuom1nen <ttuominen@outlook.com>
2021-08-24 22:27:32 +0300

    Refactor login view

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import LoginForm from "../components/LoginForm";
-import { Grid, Header, Message } from "semantic-ui-react";
+import LoginForm from "../components/LoginForm/LoginForm";
+import {
+  MainContent,
+  PaddedContainer,
+} from "../components/Common/Common.styles";
 
 const LoginView = () => {
   return (
-    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" color="teal" textAlign="center">
-          Log-in to your account
-        </Header>
+    <MainContent>
+      <PaddedContainer>
         <LoginForm></LoginForm>
-        <Message>
-          No account yet? <a href="#">Sign Up</a>
-        </Message>
-      </Grid.Column>
-    </Grid>
+      </PaddedContainer>
+    </MainContent>
   );
 };
 
