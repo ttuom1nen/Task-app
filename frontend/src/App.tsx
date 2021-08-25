87d56486e943394712fb6af0a775bc781de87ab6
ttuom1nen <ttuominen@outlook.com>
2021-08-25 22:20:22 +0300

    Remove theme switcher

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,14 @@ import MainView from "./views/MainView";
 import { PrivateRoute } from "./utils/PrivateRoute";
 import { Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-// import defaultTheme from "./themes/default.theme";
 import { defaultTheme, darkTheme } from "./themes/themes";
+import "./App.css";
 
 function App() {
   const [theme, setTheme] = useState<any>(defaultTheme);
 
-  const switchTheme = () => {
-    console.log("switchTheme");
-    setTheme(darkTheme);
-  };
-
   return (
     <ThemeProvider theme={theme}>
-      <button onClick={switchTheme}>switch</button>
       <Switch>
         <Route exact path="/login" component={LoginView} />
         <PrivateRoute path="/">
